90892aba4917d25ddd490ecfa19a00de50f1d41a
michaelkm <michael@michaelmerrill.io>
2016-03-15 14:53:14 -0700

    Add WebViewController

---
+++ MySafari/WebViewViewController.h
@@ -0,0 +1,13 @@
+//
+//  WebViewViewController.h
+//  MySafari
+//
+//  Created by Michael Merrill on 3/15/16.
+//  Copyright © 2016 Suleiman Younossi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WebViewViewController : UIViewController
+
+@end
